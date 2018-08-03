f94fe69a5742fa5defbc5e57e4eb195deb611315
github username <github email address>
2018-08-03 10:33:37 -0400

    nice

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,31 @@
 class QueueItem
 
+@@all = []
+
+attr_accessor :viewer, :movie, :rating
+
+def initialize(viewer, movie, rating=nil)
+  @viewer = viewer
+  @movie = movie
+  @rating = rating
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
 
 end
+
+
+# _A `QueueItem` object represents that an individual user has added a particular movie to their queue, and possibly rated it. **It is not the full queue, but a single item in the queue.**_
+#
+# + `QueueItem.all` - DONE
+#   + returns an array of all `QueueItem`s
+# + `QueueItem#viewer` - DONE
+#   + returns the viewer associated with this `QueueItem`
+# + `QueueItem#movie` - DONE
+#   + returns the movie associated with this `QueueItem`
+# + `QueueItem#rating` - DONE
+#   + returns the rating for this `QueueItem`. If the viewer has not yet rated the movie, `QueueItem#rating` should be `nil`
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+movie1 = Movie.new('godfather')
+movie2 = Movie.new('die hard')
+movie3 = Movie.new('up')
+movie4 = Movie.new('toy story')
+
+viewer1 = Viewer.new('random name')
+viewer2 = Viewer.new('some guy')
+viewer3 = Viewer.new('another girl')
+
+queue_item1 = QueueItem.new(viewer1, movie1, 3)
+queue_item2 = QueueItem.new(viewer2, movie1, 1)
+queue_item3 = QueueItem.new(viewer3, movie4, 1)
+queue_item4 = QueueItem.new(viewer1, movie3, 5)
+queue_item5 = QueueItem.new(viewer1, movie2, 5)
+queue_item6 = QueueItem.new(viewer1, movie4, 4)
+queue_item7 = QueueItem.new(viewer2, movie3, 3)
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
